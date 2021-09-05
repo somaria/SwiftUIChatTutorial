64b8ce7bfd741a149ee9370c56985b9bb33348cd
Daryl <daryl.9@gmail.com>
2021-09-05 19:48:11 +0800

    Initial Commit

---
+++ SwiftUIChatTutorial/SwiftUIChatTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatTutorialApp.swift
+//  SwiftUIChatTutorial
+//
+//  Created by admin on 5/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
